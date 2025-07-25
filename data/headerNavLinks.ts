f54906382206828dd6cbe36f032155c46f03485c
bbeltran <bbeltran@Bryans-MacBook-Pro.local>
2025-07-24 23:18:14 -0500

    update headerNavLinks with translation

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: '/translation', title: 'Translation' },
   { href: '/about', title: 'About' },
 ]
 
